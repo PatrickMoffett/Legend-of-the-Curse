ada71fcf8003ea2c14c677c20eefdf4c9f70b8f7
adamjsmith15 <ajsmith015@gmail.com>
2023-04-11 20:58:32 -0400

    Prefab for spike trap and trap script
    Initial Commit for Spikes coming out of the ground

---
+++ Assets/Scripts/CombatSystem/Trap.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    [SerializeField] private List<StatusEffect> effectsToApplyOnHit;
+    [SerializeField] private float trapOnTime = 0.5f;
+    [SerializeField] private float trapOffTime = 1.5f; 
+    [SerializeField] private Animator spikeAnimator;
+    [SerializeField] private bool spikeActivated;
+    [SerializeField] private bool isOnSpike;
+    [SerializeField] private string activatedAnimationName;
+    [SerializeField] private string idleAnimationName;
+    
+    private GameObject objectOnSpike;
+    
+    void Awake()
+    {
+        spikeAnimator = GetComponent<Animator>();
+        
+    }
+
+    void Start()
+    {
+        StartTrap();
+    }
+
+
+    void Update()
+    {
+        if(spikeActivated && isOnSpike) 
+        {        
+            CombatSystem combatSystem = objectOnSpike.gameObject.GetComponent<CombatSystem>();
+            if (combatSystem)
+            {
+                //Instantiate status effects
+                List<StatusEffectInstance> statusEffectInstances = new List<StatusEffectInstance>();
+                foreach (var effect in effectsToApplyOnHit)
+                {
+                    statusEffectInstances.Add(new StatusEffectInstance(effect,combatSystem));
+                }
+            }
+        }
+    }
+    
+    public void StartTrap()
+    {
+        StopAllCoroutines();
+        spikeActivated = true;
+        spikeAnimator.Play(activatedAnimationName);
+        StartCoroutine(ResetTrap());
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider){
+        isOnSpike = true;
+        objectOnSpike = collider.gameObject;
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        isOnSpike = false;
+        objectOnSpike = null;
+    }
+    
+    private IEnumerator ResetTrap()
+    {
+        yield return new WaitForSeconds(trapOnTime);
+        spikeActivated = false;
+        spikeAnimator.Play(idleAnimationName);
+        yield return new WaitForSeconds(trapOffTime);
+        StartTrap();
+    }
+}

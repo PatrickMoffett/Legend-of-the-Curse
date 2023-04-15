8724faf0dae0dec7b5c3c16212f7de79690aeb6b
Patrick Moffett <Patrick@moffett.dev>
2023-04-15 13:24:48 -0700

    Added AudioEvent Preview Button

--- Assets/Scripts/Utilities/AudioEvent/AudioEvent.cs
+++ Assets/Scripts/Utilities/AudioEvent/AudioEvent.cs
@@ -3,4 +3,6 @@ using UnityEngine;
 public abstract class AudioEvent : ScriptableObject
 {
     public abstract void Play(Vector3 position);
+
+    public abstract void Preview(AudioSource previewer);
 }
--- Assets/Scripts/Utilities/AudioEvent/SimpleAudioEvent.cs
+++ Assets/Scripts/Utilities/AudioEvent/SimpleAudioEvent.cs
@@ -1,6 +1,7 @@
-using UnityEngine;
 using Services;
+using UnityEngine;
 using Random = UnityEngine.Random;
+
 
 [CreateAssetMenu(menuName="Audio Events/Simple")]
 public class SimpleAudioEvent : AudioEvent
@@ -23,6 +24,20 @@ public class SimpleAudioEvent : AudioEvent
         AudioClip clip = clips[Random.Range(0, clips.Length)];
         float volume_ = Random.Range(volume.minValue, volume.maxValue);
         float pitch_ = Random.Range(pitch.minValue, pitch.maxValue);
+        
         ServiceLocator.Instance.Get<AudioManager>().PlaySfxAtLocation(clip, position, volume_, pitch_, spatialBlend);
+    }
+
+    public override void Preview(AudioSource previewer)
+    {
+        AudioClip clip = clips[Random.Range(0, clips.Length)];
+        float volume_ = Random.Range(volume.minValue, volume.maxValue);
+        float pitch_ = Random.Range(pitch.minValue, pitch.maxValue);
+        
+        previewer.clip = clip;
+        previewer.volume = volume_;
+        previewer.pitch = pitch_;
+        previewer.spatialBlend = spatialBlend;
+        previewer.Play();
     }
 }

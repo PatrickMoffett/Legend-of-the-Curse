dd99ec546ce61c16d1154bc413b2e004dbe0d56e
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-05-15 19:30:14 -0500

    subscribing spriteflash script to OnhealthChanged

--- Assets/Scripts/Characters/Shared/Character.cs
+++ Assets/Scripts/Characters/Shared/Character.cs
@@ -8,10 +8,20 @@ public class Character : MonoBehaviour
     protected CombatSystem CombatSystem;
     protected AttributeSet AttributeSet;
     protected CharacterMovement CharacterMovement;
+    protected SpriteFlash SpriteFlash; 
+    
     protected virtual void Start()
     {
         CharacterMovement = GetComponent<CharacterMovement>();
         CombatSystem = GetComponent<CombatSystem>();
         AttributeSet = GetComponent<AttributeSet>();
+        SpriteFlash = GetComponent<SpriteFlash>();
+
+        AttributeSet.currentHealth.OnValueChanged += OnHealthChanged;
+    }
+
+    protected void OnHealthChanged(ModifiableAttributeValue value)
+    {
+        SpriteFlash.Flash();
     }
 }

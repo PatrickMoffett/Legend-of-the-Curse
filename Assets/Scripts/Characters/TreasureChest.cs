5f17509451aa530304a1f071507691f073e58ade
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-05-16 21:17:42 -0500

    treasure chest class implementing interactable interface

---
+++ Assets/Scripts/Characters/TreasureChest.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreasureChest : MonoBehaviour, IInteractable
+{
+    public string Prompt { get; set; }
+    public bool InteractionEnabled { get; set; }
+    public void ReceiveInteraction(Character character)
+    {
+        Debug.Log("interaction received");
+        Debug.Log("treasure chest opened");
+        InteractionEnabled = false;
+    }
+}

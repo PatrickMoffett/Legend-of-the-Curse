1439937e8fae8fc3c0916387fbc4ab901daf3fe7
Michael Tanner <66394061+michael-tanner-coder@users.noreply.github.com>
2023-05-22 19:40:53 -0500

    interaction radius script gives interactions and sets UI prompts

--- Assets/Scripts/Characters/Interactions/InteractionRadius.cs
+++ Assets/Scripts/Characters/Interactions/InteractionRadius.cs
@@ -2,17 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.InputSystem;
+using TMPro;
 
 public class InteractionRadius : MonoBehaviour
 {
-    public Character character;
+    [Header("DETECTION SETTINGS")]
     public List<IInteractable> interactables = new List<IInteractable>();
     public float detectionDistance = 2f;
 
+    [Header("UI")]
+    [SerializeField] private string _promptText;
 
-    void Start()
+    void Awake()
     {
-        character = GetComponent<Character>();
+        SetPrompt(_promptText);
     }
 
     void Update()
@@ -37,9 +41,28 @@ public class InteractionRadius : MonoBehaviour
                     interactable.InteractionEnabled
                 )
             {
-                Debug.Log("Interactable found");
+                // add interactable to a list of interactables that is treated as a stack (last in, first out)
                 interactables.Add(interactable);
+
+                // signal UI to display interaction prompt
+                SetPrompt(interactable.Prompt);
             }
         }
+
+        // reset UI when no interactable is near
+        if (interactables.Count == 0)
+        {
+            SetPrompt("");
+        }
+    }
+
+    public void GiveInteraction(GameObject interactor)
+    {
+        interactables.Last().ReceiveInteraction(gameObject);
+    }
+
+    void SetPrompt(string prompt)
+    {
+        _promptText = prompt;
     }
 }
